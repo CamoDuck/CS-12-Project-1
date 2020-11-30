9532a960cbff8b7f6b6b4e562260be4df13cb94a
GAMER-PC\cubeb <cubebox52@gmail.com>
2020-11-29 21:51:17 -0800

    11/29/2020 - Added basic player movement

---
+++ CS-12-Project-1/Assets/Bow 1/ShootBow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBow : MonoBehaviour {
+    int reloadTime = 1;
+    float arrowSpeed = 0.1f;
+    float angle;
+    Vector3 mousePos = Input.mousePosition;
+    Vector3 bowPos;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        mousePos.z = 0;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        bowPos = transform.Find("Bow1_loaded").position;
+
+
+
+    }
+}
---
+++ CS-12-Project-1/Assets/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    float xSpeed;
+    float ySpeed;
+
+    void Start() {
+        Debug.Log("run MovePlayer");
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            ySpeed += 0.01f;
+        }
+        else if (Input.GetKeyUp(KeyCode.W)) {
+            ySpeed += -0.01f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            ySpeed += -0.01f;
+        }
+        else if (Input.GetKeyUp(KeyCode.S)) {
+            ySpeed += 0.01f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            xSpeed += 0.01f;
+        }
+        else if (Input.GetKeyUp(KeyCode.D)) {
+            xSpeed += -0.01f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            xSpeed += -0.01f;
+        }
+        else if (Input.GetKeyUp(KeyCode.A)) {
+            xSpeed += 0.01f;
+        }
+
+        gameObject.transform.position += new Vector3(xSpeed, ySpeed, 0); 
+    }
+}
