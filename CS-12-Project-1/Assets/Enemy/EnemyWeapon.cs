31580cbd56bedfd3d2b3ab9aca2fa17fabca4ce1
GAMER-PC\cubeb <cubebox52@gmail.com>
2020-12-03 19:42:26 -0800

    12/3/2020 - Added basic enemy AI

--- CS-12-Project-1/Assets/Enemy/EnemyWeapon.cs
+++ CS-12-Project-1/Assets/Enemy/EnemyWeapon.cs
@@ -9,21 +9,18 @@ public class EnemyWeapon : MonoBehaviour {
     IEnumerator createArrow()
     {
         Transform clone;
-        Debug.Log("ran");
         clone = Instantiate(T_Arrow, transform.position, T_Arrow.rotation);
-        clone.GetComponent<ArrowMove>().enabled = true;
+        clone.GetComponent<EnemyArrowMove>().enabled = true;
         T_Arrow.gameObject.GetComponent<SpriteRenderer>().enabled = false;
         Destroy(clone.gameObject, 3);
         yield return new WaitForSeconds(reloadTime);
         T_Arrow.gameObject.GetComponent<SpriteRenderer>().enabled = true;
-        yield return new WaitForSeconds(Random.Range(0f, 3f));
-        Debug.Log("ran2");
+        yield return new WaitForSeconds(Random.Range(0.0f, 3.0f));
         StartCoroutine(createArrow());
     }
 
     void Start() {
         T_Arrow = transform.Find("Arrow");
-        Debug.Log("ran3");
         StartCoroutine(createArrow());
     }
 
---
+++ CS-12-Project-1/Assets/Enemy/MoveEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    float speed = 5f;
+    GameObject player;
+
+    void moveAway() {
+        float Xdir = Vector3.Normalize(transform.position - player.transform.position).x;
+        float Ydir = Vector3.Normalize(transform.position - player.transform.position).y;
+        transform.position += Vector3.Normalize(new Vector3(Random.Range(Xdir-1.0f, Xdir), Random.Range(Ydir-1.0f, Ydir), 0)) * speed * Time.deltaTime;
+    }
+    void moveToward() {
+        float Xdir = Vector3.Normalize(player.transform.position - transform.position).x;
+        float Ydir = Vector3.Normalize(player.transform.position - transform.position).y;
+        transform.position += Vector3.Normalize(new Vector3(Random.Range(Xdir-1.0f, Xdir), Random.Range(Ydir-1.0f, Ydir), 0)) * speed * Time.deltaTime;
+    }
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        //Debug.Log("running2");
+        if (Vector3.Distance(transform.position, player.transform.position) < 4)
+        {
+            moveAway();
+        }
+        else if (Vector3.Distance(transform.position, player.transform.position) > 5)
+        {
+            moveToward();
+        }
+    }
+}
