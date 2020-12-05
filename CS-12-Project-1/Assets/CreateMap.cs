74e8ef1c68f512378a777846e423bc2e32c785eb
GAMER-PC\cubeb <cubebox52@gmail.com>
2020-12-04 21:11:11 -0800

    12/4/2020 - Added Crude Map Generation

---
+++ CS-12-Project-1/Assets/CreateMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour {
+
+    int negplus1() {
+        if (Random.Range(0, 2) == 0) {
+            return -1;
+        }
+        return 1;
+    } 
+    GameObject generate(GameObject lastRoom=null) {
+        GameObject clone = Instantiate(Resources.Load("Floor")) as GameObject;
+        clone.transform.localScale = new Vector3(Random.Range(20, 100), Random.Range(20,100), 1);
+        Debug.Log(clone.transform.localScale.x + lastRoom.transform.localScale.x);
+        clone.transform.position = lastRoom.transform.position + new Vector3((clone.transform.localScale.x+lastRoom.transform.localScale.x)*negplus1()*0.05f, (clone.transform.localScale.y + lastRoom.transform.localScale.y) * negplus1()*0.05f,0);
+        return clone;
+    }
+
+    //linked list to solve stacking problem =)
+
+    void Start() {
+        GameObject room = generate(GameObject.Find("start"));
+        for (int i=0; i < 10000; i++) {
+            room = generate(room);
+        }
+
+    }
+}
---
+++ CS-12-Project-1/Assets/Player/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    float xSpeed;
+    float ySpeed;
+    float speed = 5f;
+
+    void Start() {
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            ySpeed += speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.W)) {
+            ySpeed += -speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            ySpeed += -speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.S)) {
+            ySpeed += speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            xSpeed += speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.D)) {
+            xSpeed += -speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            xSpeed += -speed;
+        }
+        else if (Input.GetKeyUp(KeyCode.A)) {
+            xSpeed += speed;
+        }
+
+        transform.position += new Vector3(xSpeed, ySpeed, 0) * Time.deltaTime; 
+    }
+}
